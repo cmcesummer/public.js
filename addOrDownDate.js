8b8fb13d29582c814a7dc8d0156775eaa6399d78
zhangh <740586220@qq.com>
2016-12-22 12:04:34 +0800

    增减日期

--- addOrDownDate.js
+++ addOrDownDate.js
@@ -1,6 +1,5 @@
 var time = new Date();
-function changeDate(date, day, type) {
-	// type不传或其他情况为加  type = ‘-’ 为减日期
+function changeDate(date, day) {
 	var reg = /^\d{4}-\d{2}-\d{2}$/,
 		newDate,   
 		changeNewDate, //改变之后的date（未转换）
@@ -17,7 +16,7 @@ function changeDate(date, day, type) {
 	return y + '-' + m + '-' + d
 }
 
-console.log(changeDate('2016-12-14', 1))
+console.log(changeDate('2016-12-14', -1))
 
 // setDate(), setMonth() 比较方便，不用考虑在临界值的时候的情况
 // 要注意 setDate 或者 setMonth 之后返回的是设置好的 毫秒数， 再用new Date 转换成标准格式
