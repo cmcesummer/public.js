5aa45fb40958223eaaf9226e5f988c51cfee6b23
zhangh <740586220@qq.com>
2016-12-26 14:34:00 +0800

    Promise

--- knowledge/promise.js
+++ knowledge/promise.js
@@ -5,6 +5,9 @@ $ready(function() {
 	//js 如果遇到一个执行时间很长的东西不能让他阻塞，手动让它异步，
 	//比如 f1()耗时很长 ，f2还需要f1的结果，最简单的方法：
 	//先改写f1()
+	function f2() {
+		console.log('这是回调');
+	}
 	function f1(callback) {
 		setTimeout(function() {
 			//f1的原任务代码
