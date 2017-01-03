d75d6026b5b6a4d8015610ecdbbd946944aaaba7
zhangh <740586220@qq.com>
2017-01-03 12:00:05 +0800

    generator 自动管理流程

--- knowledge/generator.js
+++ knowledge/generator.js
@@ -27,3 +27,66 @@ var Thunk = fn => {
 
 var fs = Thunk(fs.readFile);
 fs('a.text')(callback)
+
+//--start
+//Thunk自动流程管理
+let run = fn => {  // fn 是generator
+	let g = fn();
+
+	let next = (err, data) => {
+		//先移动到下一步
+		var result = g.next(data);
+		if(result.done) return;
+		//有下一步的话， 调用回调
+		resule.value(next);
+	}
+}
+
+function* g(){
+	//...var r1 = yield readFile('/etc/fstab');  这里yield后面只能是跟 Thunk 函数
+}
+
+next(g)
+//--end
+
+//--start
+//基于Promise的对象自动执行
+//var fs = require('fs');
+var readFile = function(filename) {
+	return new Promise(function(resolve, reject) {
+		fs.readFile(filename, function(err, data) {
+			err ? reject(err) : resolve(data)
+		})
+	})
+}
+var gen = function* () {
+	var f1 = yield readFile('f1.txt');
+	console.log(f1.toString);
+	var f2 = yield readFile('f2.txt');
+	console.log(f2.toString);
+}
+var g = gen();
+g.next().value.then(function(data) {
+	g.next(data).then(function() {
+		g.next(data);
+	})
+})
+	//上边可以换成
+function run(fn) {
+	var g = fn();
+
+	function next(data) {
+		var result = g.next(data);
+		if(result.done) return result.value;
+		result.value.then(function(data) {
+			next(data);
+		})
+	}
+	next();
+}
+run(g);	
+//--end
+
+
+
+
