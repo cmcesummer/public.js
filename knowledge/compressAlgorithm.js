6c57f08f367180bb41d176a614c7b3f14a9bb9fc
zhangh <740586220@qq.com>
2017-01-09 14:03:41 +0800

    压缩算法

--- knowledge/compressAlgorithm.js
+++ knowledge/compressAlgorithm.js
@@ -1,4 +1,5 @@
-
+//实现基本的字符串压缩算法 ，比如对字符串abbbbccccdddccccc,经过算法处理后得到的输出
+//是a1b5c4d3c6，如果处理后的字符串长度不小于原长度，则返回原串
 //function 1
 (() => {
 
@@ -43,14 +44,43 @@ let compressAlgorithm = (str) => {
 		return str
 	}
 }
-let str = 'abcde'
+let str = 'aabbbcccccdde'
+console.time(1)
 console.log(compressAlgorithm(str));
-
+console.timeEnd(1)
 })();
 
-//function  2
+//function  2  
 (() => {
 
-	
+let compressAlgorithm = (str) => {	
+	if(str.length < 3) return str;
 
+	let strArr = str.split(''),
+		arrObj = {},
+		outStr = '';
+
+	for(let item of strArr) {
+		if(!arrObj[item]) {
+			arrObj[item] = [item];
+		} else {
+			arrObj[item].push(item)
+		}
+	}
+
+	for(let key in arrObj) {
+		outStr += key + arrObj[key].length
+	}
+
+	if(str.length > outStr.length) {
+		return outStr
+	} else {
+		return str
+	}
+}
+
+let str = 'aabbbcccccdde'
+console.time(2)
+console.log(compressAlgorithm(str));
+console.timeEnd(2)             //2的性能比1好多了
 })();
