229d6a4d7cd6f2118e45de70dcc33adce27d0ab3
zhangh <740586220@qq.com>
2017-01-15 19:10:40 +0800

    commonKnowJs

--- knowledge/commonKonw.js
+++ knowledge/commonKonw.js
@@ -10,9 +10,16 @@ function onlyOne(...arg) {
 	return sum === 1
 }
 
-
+//-----
 var str = 'asdfghj';
 console.log(str.substr(1, 3))  //sdf     这个第二个参数是长度
 console.log(str.substring(1, 3)) //sd    这个第二个参数是结束位置索引（非自包含），3-1 = 2， 2是长度
+
+
+//---
+//由于浏览器演进的历史遗留问题， 在创建带有 id 属性的 DOM 元素时也会创建同名的全局变量。 
+//这就很蛋疼了
+console.log(we); //<div id="we">id</div>
+
 
 
