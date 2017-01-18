b16b5614fc8e585ddab5c02a97ad89a26427f87a
zhangh <740586220@qq.com>
2017-01-18 18:21:53 +0800

    promise 事件循环位置

--- knowledge/promise.js
+++ knowledge/promise.js
@@ -2,22 +2,43 @@
 
 $ready(function() {
 
-	//js 如果遇到一个执行时间很长的东西不能让他阻塞，手动让它异步，
-	//比如 f1()耗时很长 ，f2还需要f1的结果，最简单的方法：
-	//先改写f1()
-	function f2() {
-		console.log('这是回调');
-	}
-	function f1(callback) {
-		setTimeout(function() {
-			//f1的原任务代码
-			callback();
-		},0)
-	}
-	f1(f2);
+(function() {
+
+//js 如果遇到一个执行时间很长的东西不能让他阻塞，手动让它异步，
+//比如 f1()耗时很长 ，f2还需要f1的结果，最简单的方法：
+//先改写f1()
+function f2() {
+	console.log('这是回调');
+}
+function f1(callback) {
+	setTimeout(function() {
+		//f1的原任务代码
+		callback();
+	},0)
+}
+f1(f2);
+
+})();
 
 
+(function() {
+//用了promise,不管是同步还是异步，都会变成异步
+function getPromise() {
+	return new Promise(function(resolve, reject) {
+		if (true) {
+			var data = 2;
+			resolve(data)
+		}
+	})
+}
 
+getPromise().then(function(data){
+	console.log(data)
+})
+
+console.log(1)    //  这里先输出1，再2，再‘这是回调’  、、  promise 是在这轮事件循环结束时加上去的事件列队，setTimeout是下轮事件开始前
+
+})()
 
 
 
