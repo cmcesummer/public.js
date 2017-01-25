088c11b6001dadbe13e92bc148b925825783c759
zhangh <740586220@qq.com>
2017-01-25 23:21:52 +0800

    selector

--- native-Jquery/jqSelector.js
+++ native-Jquery/jqSelector.js
@@ -17,3 +17,12 @@ qu.prototype = {
 qu.prototype.init();
 
 })()
+
+
+//css 选择器
+//div p 选择 <div> 元素内部的所有 <p> 元素。
+//div>p  选择父元素为 <div> 元素的所有 <p> 元素。
+//这两个区别：<div> <p></p> <span><p></p></span>	</div>
+//div+p 选择紧接在 <div> 元素之后的所有 <p> 元素。
+//p:first-child	选择属于父元素的第一个子元素的每个 <p> 元素。
+//p:nth-child(2)  p:nth-last-child(1);
