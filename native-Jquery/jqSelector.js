f7ed692cb4bb21e9400d9d2e7fbda032cfbd4e74
zhangh <740586220@qq.com>
2017-01-25 23:47:01 +0800

    querySelector

--- native-Jquery/jqSelector.js
+++ native-Jquery/jqSelector.js
@@ -26,3 +26,14 @@ qu.prototype.init();
 //div+p 选择紧接在 <div> 元素之后的所有 <p> 元素。
 //p:first-child	选择属于父元素的第一个子元素的每个 <p> 元素。
 //p:nth-child(2)  p:nth-last-child(1);
+//
+//
+//对于js来讲
+
+document.getElementById('test');
+document.getElementsByTagName('input');
+document.getElementsByName('checkbox');  //获取name为checkbox的DOM节点
+
+document.getElementsByClassName('red');
+document.querySelector(); //querySelector() 方法返回文档中匹配指定 CSS 选择器的一个元素。
+document.querySelectorAll(); //如果你需要返回所有的元素，请使用 querySelectorAll() 方法替代。
