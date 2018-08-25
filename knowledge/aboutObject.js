29d6494bf49a9f1083292d5e2daee414af87bd5e
zhangh <740586220@qq.com>
2018-08-25 14:44:53 +0800

    add function

--- knowledge/aboutObject.js
+++ knowledge/aboutObject.js
@@ -10,3 +10,21 @@ let obj2 = {
 let a = Object.assign(obj1, obj2);
 
 console.log(a, obj1)
+
+
+function add() {
+    var args = [...arguments];
+    function mid() {
+        args.push(...arguments);
+        return mid
+    }
+    mid.valueOf = function() {
+        console.log(args)
+        return args.reduce((lav, curv) => lav + curv, 0)
+    }
+    return mid;
+};
+console.log(add(1)(2)(1));
+console.log(add(1,2));
+
+
